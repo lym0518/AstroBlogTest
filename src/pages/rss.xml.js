b3569cfc4c54a63f43211a95c70421662bac55e0
lym0518 <wp13068010057@163.com>
2024-08-31 11:49:31 +0800

    添加标签页面 添加rss订阅源 可根据文章标签自动生成新的标签详情页 添加暗黑模式

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,11 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
 	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
+		title: '言梦录',
+		description: '测试博客',
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
+		items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+		customData: `<language>zh-cn</language>`,
 	});
 }
